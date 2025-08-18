dcc5e32295dddf1cdcd99a9dd0c29945bf6b334d
yezihang <2653009137@qq.com>
2025-08-19 00:43:50 +0800

    dev first commit

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Welcome to the Java program.");
         System.out.println("Hello Python!");
     }
 }
